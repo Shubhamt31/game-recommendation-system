d75870a2bd8ecf6b75c9e12724ccb7fae16386d5
Shubhamt31 <shubham.t.31@gmail.com>
2024-04-04 08:58:17 +0530

    frontend first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core';
+import AdbIcon from '@mui/icons-material/Adb';
 
+// Import your page components here
+import GettingStartedPage from './pages/GettingStartedPage';
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    backgroundColor: '#cc0000',
+  },
+}));
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <AppBar position="static" className={classes.appBar}>
+          <Toolbar>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+            <Typography variant="h6">Games Scout</Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Routes>
+          <Route path="/" element={<GettingStartedPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
+ReactDOM.render(<App />, document.getElementById('root'));
 export default App;
